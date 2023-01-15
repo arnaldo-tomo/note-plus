7b96fa3d03dbce68228cab1d4031142afd31c523
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2023-01-15 22:21:07 +0200

    Update dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,13 +5,41 @@
         </h2>
     </x-slot>
 
-    <div class="py-12">
+    <div class="py-10">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
+                   {{ Auth::user()->email }}
                 </div>
             </div>
         </div>
     </div>
+    <div class="py-10">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    <div>
+                        <label for="price" class="block text-sm font-medium text-white-700">Price</label>
+                        <div class="relative mt-1 rounded-md shadow-sm">
+                          <div class="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
+                            <span class="text-gray-500 sm:text-sm">$</span>
+                          </div>
+                          <input type="text" name="price" id="price" class="block w-full rounded-md border-gray-300 pl-7 pr-12 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm" placeholder="0.00">
+                          <div class="absolute inset-y-0 right-0 flex items-center">
+                            <label for="currency" class="sr-only">Currency</label>
+                            <select id="currency" name="currency" class="h-full rounded-md border-transparent bg-transparent py-0 pl-2 pr-7 text-gray-500 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm">
+                              <option>USD</option>
+                              <option>CAD</option>
+                              <option>EUR</option>
+                            </select>
+                          </div>
+                        </div>
+                      </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
 </x-app-layout>
