de79f616ddc4231a9e3bf0912dafeeecec77829b
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2023-01-17 01:13:49 +0200

    Update index.blade.php

---
+++ resources/views/modal/index.blade.php
@@ -0,0 +1,42 @@
+    <!-- Modal -->
+    <div class="modal fade" id="create-note" tabindex="-1" role="dialog" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content">
+                <div class="modal-body">
+                    <div class="popup text-left">
+                        <h4 class="mb-4">Create New Tag</h4>
+                        <div class="content create-workform">
+                            <input type="text" class="form-control" placeholder="Enter Tag Name">
+                            <div class="col-lg-12 mt-4">
+                                <div class="d-flex flex-wrap align-items-ceter justify-content-center">
+                                    <div class="btn btn-outline-primary mr-4" data-dismiss="modal">Create</div>
+                                    <div class="btn btn-primary" data-dismiss="modal">Cancel</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Modal -->
+    <div class="modal fade" id="tag-rename" tabindex="-1" role="dialog" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content">
+                <div class="modal-body">
+                    <div class="popup text-left">
+                        <h4 class="mb-4">Rename Your Tag</h4>
+                        <div class="content create-workform">
+                            <input type="text" class="form-control" placeholder="Enter Tag Name">
+                            <div class="col-lg-12 mt-4">
+                                <div class="d-flex flex-wrap align-items-ceter justify-content-center">
+                                    <div class="btn btn-primary mr-4" data-dismiss="modal">Cancel</div>
+                                    <div class="btn btn-outline-primary" data-dismiss="modal">Save</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div> <!-- loader END -->
