2d61e8ed9c62a4b7a2e07b282a6c38e50df41120
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2023-01-17 01:05:49 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('auth.login');
+    return view('welcome');
 });
 
 Route::get('/dashboard', function () {
