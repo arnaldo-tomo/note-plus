d6a78852bdbad727a7d8b835b594e7ae478b067f
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2023-01-17 11:20:30 +0200

    Create Root.php

---
+++ app/Http/Livewire/Root.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class Root extends Component
+{
+    public function render()
+    {
+        return view('livewire.root');
+    }
+}
