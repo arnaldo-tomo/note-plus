26ada6ebd70bcf1511bffde8a73b9878f61d8277
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2023-01-17 22:41:26 +0200

    Update Index.php

--- app/Http/Livewire/Index.php
+++ app/Http/Livewire/Index.php
@@ -8,6 +8,6 @@ class Index extends Component
 {
     public function render()
     {
-        return view('livewire.index');
+        return view('livewire.index')->layout('livewire.root');
     }
 }
