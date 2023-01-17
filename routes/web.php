3a68f9c6efb2209e1846d14c115a40537b33e53e
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2023-01-17 22:41:34 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,36 +2,21 @@
 
 use App\Http\Controllers\ProfileController;
 use App\Http\Livewire\Criar;
+use App\Http\Livewire\Index;
 use App\Http\Livewire\Root;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', function () {
     return view('auth.login');
 });
 
-Route::get('welcome', Root::class)->name('welcome');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/criar', Criar::class)->name('criar');
+
 Route::middleware('auth')->group(function () {
-    // Route::get('/welcome', function () {
-    //     return view('welcome');
-    // });
-
+    Route::get('index', Index::class)->name('welcome');
+    Route::get('/criar', Criar::class)->name('criar');
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
