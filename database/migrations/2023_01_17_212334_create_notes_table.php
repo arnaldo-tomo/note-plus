e34741e8f2b7745dac7648a5d82867b2f68a2318
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2023-01-17 23:41:13 +0200

    Create 2023_01_17_212334_create_notes_table.php

---
+++ database/migrations/2023_01_17_212334_create_notes_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('notes', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id');
+            $table->foreignId('title');
+            $table->foreignId('description');
+            $table->foreignId('remenber',);
+            $table->foreignId('icon',);
+            $table->foreignId('priority');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('notes');
+    }
+};
