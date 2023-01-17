06fa347cfc19eff5f5214a708816447aca4569a8
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2023-01-17 23:41:09 +0200

    Update Criar.php

--- app/Http/Livewire/Criar.php
+++ app/Http/Livewire/Criar.php
@@ -7,10 +7,15 @@ use Livewire\Component;
 
 class Criar extends Component
 {
+    public $user_id, $title, $description, $remenber, $icon, $priority;
     
     public function render()
     {
-
         return view('livewire.criar')->layout('livewire.root');
+    }
+
+    public function storenotes()
+    {
+        dd($this->title);
     }
 }
