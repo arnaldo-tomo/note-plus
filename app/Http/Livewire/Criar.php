9f151583da25a1363af40e9fa16ef842f94b6697
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2023-01-17 11:20:28 +0200

    Create Criar.php

---
+++ app/Http/Livewire/Criar.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class Criar extends Component
+{
+    public function render()
+    {
+        return view('livewire.criar')->layout('layouts.app');
+    }
+}
