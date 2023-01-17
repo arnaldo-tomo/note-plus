8974345ac9d87b77e599bcd1cbb899a611da278b
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2023-01-17 22:41:28 +0200

    Update RouteServiceProvider.php

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/welcome';
+    public const HOME = '/index';
 
     /**
      * Define your route model bindings, pattern filters, and other route configuration.
