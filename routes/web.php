ff991d58ac32786871d649343e9a46742d7c9f5c
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2023-01-17 11:20:44 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Livewire\Criar;
+use App\Http\Livewire\Root;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,15 +19,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('auth.login');
 });
-Route::get('/welcome', function () {
-    return view('welcome');
-});
+
+Route::get('welcome', Root::class)->name('welcome');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+    // Route::get('/welcome', function () {
+    //     return view('welcome');
+    // });
+
+    Route::get('/criar', Criar::class)->name('criar');
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
