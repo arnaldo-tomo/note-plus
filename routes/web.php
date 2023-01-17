e170f77cdf8dab36a7283278125a46612b373e87
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2023-01-17 22:44:48 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -10,10 +10,8 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', [Controller::class, 'auth']);
-
-
 Route::middleware('auth')->group(function () {
-    Route::get('index', Index::class)->name('welcome');
+    Route::get('index', Index::class)->name('index');
     Route::get('/criar', Criar::class)->name('criar');
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
