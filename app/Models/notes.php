755f6818b13253cbf36c8d85cf564812a51cb50c
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2023-01-17 23:41:11 +0200

    Create notes.php

---
+++ app/Models/notes.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class notes extends Model
+{
+    use HasFactory;
+    protected $table = 'notes';
+    protected $fillable = ['user_id', 'title', 'description', 'remenber', 'icon', 'priority'];
+}
