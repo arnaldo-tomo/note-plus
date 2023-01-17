264410427b7fdfddc300269b9b945cf818f767d0
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2023-01-17 22:20:38 +0200

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,4 +1,4 @@
-<x-guest-layout>
+{{-- <x-guest-layout>
     <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
 
@@ -44,4 +44,105 @@
             </x-primary-button>
         </div>
     </form>
-</x-guest-layout>
+</x-guest-layout> --}}
+
+
+
+
+<!doctype html>
+<html lang="en">
+  
+<!-- Mirrored from templates.iqonic.design/note-plus/html/backend/auth-sign-in.html by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 15 Dec 2022 13:41:35 GMT -->
+<head>
+    <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+      <title>NotePlus | Responsive Bootstrap 4 Admin Dashboard Template</title>
+      
+      <!-- Favicon -->
+      <link rel="shortcut icon" href="https://templates.iqonic.design/note-plus/html/assets/images/favicon.ico" />
+      <link rel="stylesheet" href="../assets/css/backend-plugin.mine209.css?v=1.0.0">
+      <link rel="stylesheet" href="../assets/css/backende209.css?v=1.0.0">
+      <link rel="stylesheet" href="../assets/vendor/%40fortawesome/fontawesome-free/css/all.min.css">
+      <link rel="stylesheet" href="../assets/vendor/line-awesome/dist/line-awesome/css/line-awesome.min.css">
+      <link rel="stylesheet" href="../assets/vendor/remixicon/fonts/remixicon.css">
+      <link rel="stylesheet" href="../assets/vendor/%40icon/dripicons/dripicons.css">  </head>
+  <body class=" ">
+    <!-- loader Start -->
+    <div id="loading">
+          <div id="loading-center">
+          </div>
+    </div>
+    <!-- loader END -->
+    
+      <div class="wrapper">
+       <section class="login-content">
+         <div class="container h-100">
+            <div class="row justify-content-center align-items-center height-self-center">
+               <div class="col-md-5 col-sm-12 col-12 align-self-center">
+                  <div class="sign-user_card">   
+                     <div class="logo-detail">            
+                           <div class="d-flex align-items-center"><img src="../assets/images/logo.png" class="img-fluid rounded-normal light-logo logo" alt="logo"> <h4 class="logo-title ml-3">NotePlus</h4></div>
+                     </div>      
+                     <h3 class="mb-2">Sign In</h3>
+                     <p>Login to stay connected.</p>
+                     <form>
+                        <div class="row">
+                           <div class="col-lg-12">
+                              <div class="floating-label form-group">
+                                 <input class="floating-input form-control" type="email" placeholder=" ">
+                                 <label>Email</label>
+                              </div>
+                           </div>
+                           <div class="col-lg-12">
+                              <div class="floating-label form-group">
+                                 <input class="floating-input form-control" type="password" placeholder=" ">
+                                 <label>Password</label>
+                              </div>
+                           </div>
+                           <div class="col-lg-6">
+                              <div class="custom-control custom-checkbox mb-3 text-left">
+                                 <input type="checkbox" class="custom-control-input" id="customCheck1">
+                                 <label class="custom-control-label" for="customCheck1">Remember Me</label>
+                              </div>
+                           </div>
+                           <div class="col-lg-6">
+                              <a href="auth-recoverpw.html" class="text-primary float-right">Forgot Password?</a>
+                           </div>
+                        </div>
+                        <button type="submit" class="btn btn-primary">Sign In</button>
+                        <p class="mt-3 mb-0">
+                           Create an Account <a href="auth-sign-up.html" class="text-primary"><b>Sign Up</b></a>
+                        </p>
+                     </form>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </section>
+      </div>
+    
+    <!-- Backend Bundle JavaScript -->
+    <script src="../assets/js/backend-bundle.min.js"></script>
+    
+    <!-- Flextree Javascript-->
+    <script src="../assets/js/flex-tree.min.js"></script>
+    <script src="../assets/js/tree.js"></script>
+    
+    <!-- Table Treeview JavaScript -->
+    <script src="../assets/js/table-treeview.js"></script>
+    
+    <!-- SweetAlert JavaScript -->
+    <script src="../assets/js/sweetalert.js"></script>
+    
+    <!-- Chart Custom JavaScript -->
+    <script src="../assets/js/chart-custom.js"></script>
+    
+    <!-- slider JavaScript -->
+    <script src="../assets/js/slider.js"></script>
+    
+    <!-- app JavaScript -->
+    <script src="../assets/js/app.js"></script>
+  </body>
+
+<!-- Mirrored from templates.iqonic.design/note-plus/html/backend/auth-sign-in.html by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 15 Dec 2022 13:41:35 GMT -->
+</html>
