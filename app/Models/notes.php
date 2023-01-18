095fc554578e4a5eec7b0b3c7f271da2e1cdaef8
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2023-01-18 16:17:34 +0200

    Update notes.php

--- app/Models/notes.php
+++ app/Models/notes.php
@@ -8,6 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class notes extends Model
 {
     use HasFactory;
-    protected $table = 'notes';
+    protected $table = "notes";
     protected $fillable = ['user_id', 'title', 'description', 'remenber', 'icon', 'priority'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
