f653d4b8890914b316ee6e9f824531ccea590df8
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2023-01-18 18:49:56 +0200

    Update Index.php

--- app/Http/Livewire/Index.php
+++ app/Http/Livewire/Index.php
@@ -2,12 +2,17 @@
 
 namespace App\Http\Livewire;
 
+use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 
 class Index extends Component
 {
     public function render()
     {
+        if (!Auth::check()) {
+            return redirect()->back();
+        } else {
         return view('livewire.index')->layout('livewire.root');
     }
+}
 }
