00f406cdd681c11321564818b4563ef9d0b48699
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2023-01-19 02:46:39 +0200

    Update Index.php

--- app/Http/Livewire/Index.php
+++ app/Http/Livewire/Index.php
@@ -5,15 +5,18 @@ namespace App\Http\Livewire;
 use App\Models\notes;
 use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class Index extends Component
 {
+    use WithPagination;
+    protected $paginationTheme = 'bootstrap';
     public function render()
     {
         if (!Auth::check()) {
             return redirect()->back();
         } else {
-            $notes = notes::all();
+            $notes = notes::ORDERBY('id', 'DESC')->paginate(4);
             return view('livewire.index', compact('notes'))->layout('livewire.root');
     }
 }
