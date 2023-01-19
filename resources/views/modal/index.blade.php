e3410585b2bc5f6f69012176aacc4cf9f9bd5b2c
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2023-01-19 14:22:48 +0200

    Update index.blade.php

--- resources/views/modal/index.blade.php
+++ resources/views/modal/index.blade.php
@@ -1,10 +1,10 @@
-     <div class="modal fade" id="new-note2" tabindex="-1" role="dialog" aria-hidden="true">
+     <div wire:ignore.self class="modal fade" id="new-note2" tabindex="-1" role="dialog" aria-hidden="true">
         <div class="modal-dialog modal-dialog-centered" role="document">
             <div class="modal-content">
                 <div class="modal-body">
                     <div class="popup text-left">
                         <div class="media align-items-top justify-content-between">                            
-                            <h3 class="mb-3">{{ $note->title }}</h3>
+                            <h3 class="mb-3">{{ $note->title }} nado</h3>
                             <div class="media align-items-center">
                                 <a href="#" data-toggle="tooltip" data-placement="top" title="" data-original-title="pin content"><i class="las la-thumbtack mr-2"></i></a>
                                 <div class="btn-cancel p-0" data-dismiss="modal"><i class="las la-times"></i></div>
