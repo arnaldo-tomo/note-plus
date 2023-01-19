f110af3b9ebc71efaf06cadb7d644d0f1a7b385a
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2023-01-19 02:37:13 +0200

    Update index.blade.php

--- resources/views/livewire/index.blade.php
+++ resources/views/livewire/index.blade.php
@@ -313,6 +313,33 @@
                                                                     stroke-width="2"
                                                                     d="M5 8h14M5 8a2 2 0 110-4h14a2 2 0 110 4M5 8v10a2 2 0 002 2h10a2 2 0 002-2V8m-9 4h4" />
                                                                        @break
+
+                                                                       @case('option4')
+                                                                       <path stroke-linecap="round" stroke-linejoin="round"
+                                                                       stroke-width="2"
+                                                                       d="M21 15.546c-.523 0-1.046.151-1.5.454a2.704 2.704 0 01-3 0 2.704 2.704 0 00-3 0 2.704 2.704 0 01-3 0 2.704 2.704 0 00-3 0 2.704
+                                                                        2.704 0 01-3 0 2.701 2.701 0 00-1.5-.454M9 6v2m3-2v2m3-2v2M9
+                                                                         3h.01M12 3h.01M15 3h.01M21 21v-7a2 2 0 00-2-2H5a2 2 0 00-2 2v7h18zm-3-9v-2a2 2 0 00-2-2H8a2 2 0 00-2 2v2h12z" />
+                                                                       @break
+                                                                       @case('option5')
+                                                                       <path stroke-linecap="round" stroke-linejoin="round"
+                                                                       stroke-width="2"
+                                                                       d="M3 9a2 2 0 012-2h.93a2 2 0 001.664-.89l.812-1.22A2 2 0 0110.07
+                                                                        4h3.86a2 2 0 011.664.89l.812 1.22A2 2 0 0018.07 7H19a2 2 0 012 2v9a2 2 0 01-2 2H5a2 2 0 01-2-2V9z" />
+                                                                       @break
+                                                                       @case('option6')
+                                                                       <path stroke-linecap="round" stroke-linejoin="round"
+                                                                       stroke-width="2"
+                                                                       d="M8 10h.01M12 10h.01M16 10h.01M9 16H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-5l-5 5v-5z" />
+                                                                       @break
+                                                                       @case('option7')
+                                                                       <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
+                                                                       @break
+                                                                       @case('option8')
+                                                                       <path stroke-linecap="round" stroke-linejoin="round"
+                                                                       stroke-width="2"
+                                                                       d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                                                                       @break
                                                                
                                                                    @default
                                                                        
