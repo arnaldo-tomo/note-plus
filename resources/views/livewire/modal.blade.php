712402bdc1ee6c9b564f6584a60ff6ef80f56d3c
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2023-01-21 14:21:53 +0200

    Update modal.blade.php

--- resources/views/livewire/modal.blade.php
+++ resources/views/livewire/modal.blade.php
@@ -1,23 +1,57 @@
-   <div wire:ignore.self class="modal fade" id="new-note2" tabindex="-1" role="dialog" aria-hidden="true">
-        <div class="modal-dialog modal-dialog-centered" role="document">
-            <div class="modal-content">
-                <div class="modal-body">
-                    <div class="popup text-left">
-                        <div class="media align-items-top justify-content-between">                            
-                            <h3 class="mb-3">{{ $note->title }} nado</h3>
-                            <div class="media align-items-center">
-                                <a href="#" data-toggle="tooltip" data-placement="top" title="" data-original-title="pin content"><i class="las la-thumbtack mr-2"></i></a>
-                                <div class="btn-cancel p-0" data-dismiss="modal"><i class="las la-times"></i></div>
-                            </div>
-                        </div>
-                        <div class="content create-workform">
-                            <p>You can easily share via message, WhatsApp, emails etc. You can also save your notes and edit it later or can easily delete the note.</p> 
-                            <ul class="pl-3 mb-0">
-                                <li class="note-list">Cakes and Balloons.</li>
-                            </ul>
-                        </div>
+<div wire:ignore.self class="modal fade" id="new-note2" tabindex="-1" role="dialog" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+            <div class="modal-body">
+                <div class="popup text-left">
+                    <div class="media align-items-top justify-content-between">
+
+<h3 class="mb-3">nado44</h3>
+<div class="media align-items-center">
+    <a href="#" data-toggle="tooltip" data-placement="top" title="" data-original-title="pin content"><i class="las la-thumbtack mr-2"></i></a>
+    <div class="btn-cancel p-0" data-dismiss="modal"><i class="las la-times"></i></div>
+</div>
+
+
+
+
                     </div>
-                </div>
-            </div>
-        </div>
-    </div> 
+                    <div class="content create-workform">
+
+                        <p>You can easily share via message, WhatsApp, emails etc. You can also save your notes and edit
+                            it later or can easily delete the note.</p>
+                        <ul class="pl-3 mb-0">
+                            <li class="note-list">Cakes and Balloons.</li>
+                        </ul>
+
+
+
+
+                    </div>
+                    </div>
+                    </div>
+                    </div>
+                    </div>
+                    </div>
+
+@push('component-scripts')
+
+<script>
+    window.addEventListener('mostrar', event => {
+        alert();
+        $('#new-note2').modal('show');
+
+    });
+
+</script>
+{{-- <script>
+    (function($) { $(document).on('livewire:load', function() {
+            
+                        Livewire.on('close-modal', (modalId) => {
+                    $(modalId).modal('hide')
+            })
+            })
+            
+                    })(jQuery)
+</script> --}}
+@endpush
+
