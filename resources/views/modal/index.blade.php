e4abd3076e8117c525ffcc73dec573e0ca35e440
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2023-01-22 12:13:50 +0200

    Update index.blade.php

--- resources/views/modal/index.blade.php
+++ resources/views/modal/index.blade.php
@@ -1,55 +1,77 @@
 <div wire:ignore.self class="modal fade" id="new-note20" tabindex="-1" role="dialog" aria-hidden="true">
-        <div class="modal-dialog modal-dialog-centered" role="document">
-            <div class="modal-content">
-                <div class="modal-body">
-                    <div class="popup text-left">
-                        <div class="media align-items-top justify-content-between">                            
-                            <h3 class="mb-3">{{ $note->title }} nado</h3>
-                            <div class="media align-items-center">
-                                <a href="#" data-toggle="tooltip" data-placement="top" title="" data-original-title="pin content"><i class="las la-thumbtack mr-2"></i></a>
-                                <div class="btn-cancel p-0" data-dismiss="modal"><i class="las la-times"></i></div>
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+            <div class="modal-body">
+                <div class="popup text-left">
+                    <div class="media align-items-top justify-content-between">
+                        <h3 class="mb-3">{{ $note->title }} nado</h3>
+                        <div class="media align-items-center">
+
+
+
+
+                            <a href="#" data-toggle="tooltip" data-placement="top" title="" data-original-title="pin content"><i class="las la-thumbtack mr-2"></i></a>
+                            <div class="btn-cancel p-0" data-dismiss="modal"><i class="las la-times"></i></div>
+
+
+
+
                             </div>
+                            </div>
+                            <div class="content create-workform">
+
+<p>You can easily share via message, WhatsApp, emails etc. You can also save your notes and edit
+    it later or can easily delete the note.</p>
+<ul class="pl-3 mb-0">
+    <li class="note-list">Cakes and Balloons.</li>
+</ul>
+
+
+
+
                         </div>
-                        <div class="content create-workform">
-                            <p>You can easily share via message, WhatsApp, emails etc. You can also save your notes and edit it later or can easily delete the note.</p> 
-                            <ul class="pl-3 mb-0">
-                                <li class="note-list">Cakes and Balloons.</li>
-                            </ul>
                         </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div> 
-    <!-- Modal -->
-    <div class="modal fade" id="create-note" tabindex="-1" role="dialog" aria-hidden="true">
-        <div class="modal-dialog modal-dialog-centered" role="document">
-            <div class="modal-content">
-                <div class="modal-body">
-                    <div class="popup text-left">
-                        <h4 class="mb-4">Create New Tag</h4>
-                        <div class="content create-workform">
-                            <input type="text" class="form-control" placeholder="Enter Tag Name">
-                            <div class="col-lg-12 mt-4">
-                                <div class="d-flex flex-wrap align-items-ceter justify-content-center">
-                                    <div class="btn btn-outline-primary mr-4" data-dismiss="modal">Create</div>
-                                    <div class="btn btn-primary" data-dismiss="modal">Cancel</div>
+                        </div>
+                        </div>
+                        </div>
+                        </div>
+                        <!-- Modal -->
+                        <div class="modal fade" id="create-note" tabindex="-1" role="dialog" aria-hidden="true">
+                            <div class="modal-dialog modal-dialog-centered" role="document">
+                                <div class="modal-content">
+                                    <div class="modal-body">
+                                        <div class="popup text-left">
+                                            <h4 class="mb-4">Create New Tag</h4>
+                                            <div class="content create-workform">
+                                                <input type="text" class="form-control" placeholder="Enter Tag Name">
+                                                <div class="col-lg-12 mt-4">
+                                                    <div class="d-flex flex-wrap align-items-ceter justify-content-center">
+                                                        <div class="btn btn-outline-primary mr-4" data-dismiss="modal">Create</div>
+                                                        <div class="btn btn-primary" data-dismiss="modal">Cancel</div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <!-- Modal -->
-  
+                        <!-- Modal -->
 
-     
 
-   
 
-  <!-- Modal Edit -->
+
+
+
+
+
+
+
+<!-- Modal Edit -->
+
+
+
+
 <div wire:ignore.self class="modal fade" id="modaleditar" tabindex="-1" role="dialog" aria-hidden="true">
 
 
@@ -57,9 +79,15 @@
     <div class="modal-dialog modal-dialog-centered" role="document">
         <div class="modal-content">
             <div class="modal-body">
+                <form wire:submit.prevent="atualizar">
+
+
                 <div class="popup text-left">
-                    <div class="media align-items-top justify-content-between">                            
-                        <h3 class="mb-3">{{ $editarTitle }}</h3>
+                    <div class="media align-items-top justify-content-between">
+
+<input wire:model="autualizarTitle" value="{{ $editarTitle }}" class="form-control">
+
+
 
 
                         <div class="btn-cancel p-0" data-dismiss="modal"><i class="las la-times"></i></div>
@@ -67,12 +95,18 @@
                     <div class="content edit-notes">
                         <div class="card card-transparent card-block card-stretch event-note mb-0">
                             <div class="card-body px-0 bukmark">
-                                <div class="d-flex align-items-center justify-content-between pb-2 mb-3 border-bottom">                                                    
+                                <div class="d-flex align-items-center justify-content-between pb-2 mb-3 border-bottom">
+
+
+
+
                                     <div class="quill-tool">
                                     </div>
                                 </div>
                                 <div id="quill-toolbar1">
-                            <textarea id="quill-toolbar1" class="form-control" cols="30" rows="5">{{ $editarDescription }}</textarea>
+                                    <textarea id="quill-toolbar1" wire:model="actulaizarDescription" class="form-control" cols="30" rows="5">{{ $editarDescription }}</textarea>
+
+
 
 
                                 </div>
@@ -80,13 +114,21 @@
                             <div class="card-footer border-0">
                                 <div class="d-flex flex-wrap align-items-ceter justify-content-end">
                                     <div class="btn btn-primary mr-3" data-dismiss="modal">Fechar</div>
-                                    <div class="btn btn-outline-primary" data-dismiss="modal">Actular notes</div>
+                                    <div class="btn btn-outline-primary" data-dismiss="modal">
+                                        <div wire:loading> <i class="fa fa-spinner fa-pulse fa-1x fa-fw"> </i> </div>
+                                        Actular notes
+                                    </div>
+
+
 
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
+                </form>
+
+
             </div>
         </div>
     </div>
@@ -100,15 +142,20 @@
                 <div class="popup text-left">
                     <div class="media align-items-top justify-content-between">
 
-                        <h3 class="mb-3">{{ $this->title_ver  }}</h3>
+                        <h3 class="mb-3">{{ $this->title_ver }}</h3>
+
+
 
                         <div class="media align-items-center">
                             <a href="#" data-toggle="tooltip" data-placement="top" title="" data-original-title="pin content"><i class="las la-thumbtack mr-2"></i></a>
+
                             <div class="btn-cancel p-0" data-dismiss="modal"><i class="las la-times"></i></div>
                         </div>
                     </div>
                     <div class="content create-workform">
-                        <p>{{ $description_ver}}</p>
+                        <p>{{ $description_ver }}</p>
+
+
 
                     </div>
                 </div>
@@ -132,6 +179,7 @@
                             <input type="text" class="form-control" placeholder="" value="https://dribble.com/Shots/6387620">
 
 
+
                         </div>
                         <div class="pb-3">
                             <h5 class="mb-2">Invite Someone</h5>
@@ -150,44 +198,72 @@
     </div>
 </div>
 
- <!-- confirmar delete -->
-
- <div class="modal fade bd-example-modal-sm" tabindex="-1" role="dialog" aria-hidden="true">
-     <div class="modal-dialog modal-sm">
-         <div class="modal-content">
-             <div class="modal-header bg-danger rounded-0 ">
-
-                 <h5 class="modal-title text-white text-center"><strong class="text-center">Deletar esta nota</strong></h5>
-
-                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                     <span aria-hidden="true">&times;</span>
-                 </button>
-             </div>
-             <div class="modal-body  rounded-0 ">
-
-                 <Strong><i class="fa fa-tasks"></i> {{ $titleDelete }}</Strong>
+<!-- confirmar delete -->
 
 
 
 
 
+<div class="modal fade bd-example-modal-sm" tabindex="-1" role="dialog" aria-hidden="true">
+    <div class="modal-dialog modal-sm">
+        <div class="modal-content">
+            <div class="modal-header bg-danger rounded-0 ">
 
 
-             </div>
-             <div class="modal-footer  rounded-0">
-
-
-                 <button type="button" wire:click='delete({{ $idDelete }})' class="btn btn-primary btn-block rounded-0">
-                     <div wire:loading> <i class="fa fa-spinner fa-pulse fa-1x fa-fw"> </i> </div> sim
-                 </button>
-
+<h5 class="modal-title text-white text-center"><strong class="text-center">Deletar esta nota</strong>
+</h5>
 
 
 
-             </div>
-         </div>
-     </div>
- </div>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+                </div>
+                <div class="modal-body  rounded-0 ">
+
+
+
+
+
+                <Strong><i class="fa fa-tasks"></i> {{ $titleDelete }}</Strong>
+
+
+
+
+
+
+
+
+
+
+
+            </div>
+            <div class="modal-footer  rounded-0">
+
+
+
+
+
+
+                <button type="button" wire:click='delete({{ $idDelete }})' class="btn btn-primary btn-block rounded-0">
+                    <div wire:loading> <i class="fa fa-spinner fa-pulse fa-1x fa-fw"> </i> </div> sim
+                </button>
+
+
+
+
+
+
+
+
+            </div>
+            </div>
+            </div>
+            </div>
+
+
+
+
 
 
 
