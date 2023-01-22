591eb706be7a8f2157e2f6b657aad707134b00e3
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2023-01-22 11:06:52 +0200

    Update notes.php

--- app/Models/notes.php
+++ app/Models/notes.php
@@ -4,11 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Livewire\WithPagination;
 
 class notes extends Model
 {
     use HasFactory;
+    use SoftDeletes;
     protected $table = "notes";
     protected $fillable = ['user_id', 'title', 'description', 'remenber', 'icon', 'priority'];
 
