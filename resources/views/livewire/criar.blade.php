f2fe795e89028a58fe10c9cc0ceef0e7f6a395ca
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2023-01-22 13:36:51 +0200

    Update criar.blade.php

--- resources/views/livewire/criar.blade.php
+++ resources/views/livewire/criar.blade.php
@@ -415,6 +415,8 @@
                                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                                     d="M8 7H5a2 2 0 00-2 2v9a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-3m-1 4l-3 3m0 0l-3-3m3 3V4" />
                                             </svg>
+                                            <div wire:loading> <i class="fa fa-spinner fa-pulse fa-1x fa-fw"> </i>
+                                            </div>
                                             Guardar Nota
                                         </button>
                                     </form>
